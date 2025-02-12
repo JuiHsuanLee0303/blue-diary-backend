ffaed45ac7924be07ba6d69109b02acbbe277307
JuiHsuanLee0303 <juihsuanlee0303@gmail.com>
2025-02-13 02:44:46 +0800

    fix: handle vercel.json

--- api/index.js
+++ api/index.js
@@ -1,2 +1,8 @@
 const app = require("../src");
-module.exports = app;
+
+// 確保導出正確的處理函數
+module.exports = (req, res) => {
+  // 在 Vercel 環境中，req 和 res 是原生的 Node.js http 對象
+  // 需要將它們傳遞給 Express 應用
+  return app(req, res);
+};
